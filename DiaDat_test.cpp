f2495d1f4aa647eed9239a0218211cf12b38c459
Gabor Liptak <liptakgabor@freemail.hu>
2021-07-11 10:31:36 +0200

    REAL32 handling is implemented REAL64 testing is added

--- DiaDat_test.cpp
+++ DiaDat_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <stdio.h>
+#include <math.h>
 #include <assert.h>
 
 #include "DiaDat_file.h"
@@ -45,6 +46,14 @@ int main(int argc, const char **argv)
       file->createChannel("sin_u32", e_DiaDat_ChannelType_u32, &sin_u32);
       uint32_t cos_u32 = 1e6;
       file->createChannel("cos_u32", e_DiaDat_ChannelType_u32, &cos_u32);
+      float sin_r32 = 1.5;
+      file->createChannel("sin_r32", e_DiaDat_ChannelType_f32, &sin_r32);
+      float cos_r32 = 2.2;
+      file->createChannel("cos_r32", e_DiaDat_ChannelType_f32, &cos_r32);
+      double sin_r64 = 5.5;
+      file->createChannel("sin_r64", e_DiaDat_ChannelType_d64, &sin_r64);
+      double cos_r64 = 6.2;
+      file->createChannel("cos_r64", e_DiaDat_ChannelType_d64, &cos_r64);
       file->set_dT(0.01);
       file->step();
       sin_u8 = 5;
@@ -59,6 +68,10 @@ int main(int argc, const char **argv)
       cos_s32 = 129000;
       sin_u32 = 1e7;
       cos_u32 = 1e8;
+      sin_r32 = 3.3;
+      cos_r32 = 4.4;
+      sin_r64 = 7.7;
+      cos_r64 = -8.8;
       file->step();
       file->close();
 
@@ -76,6 +89,10 @@ int main(int argc, const char **argv)
       file->connectVar("cos_s32", &cos_s32);
       file->connectVar("sin_u32", &sin_u32);
       file->connectVar("cos_u32", &cos_u32);
+      file->connectVar("sin_r32", &sin_r32);
+      file->connectVar("cos_r32", &cos_r32);
+      file->connectVar("sin_r64", &sin_r64);
+      file->connectVar("cos_r64", &cos_r64);
       file->step();
       assert(sin_u8 == 1);
       assert(cos_u8 == 2);
@@ -89,6 +106,10 @@ int main(int argc, const char **argv)
       assert(cos_s32 == -1111);
       assert(sin_u32 == 65538);
       assert(cos_u32 == 1e6);
+      assert(fabs(sin_r32 - 1.5) < 1e-6);
+      assert(fabs(cos_r32 - 2.2) < 1e-6);
+      assert(fabs(sin_r64 - 5.5) < 1e-6);
+      assert(fabs(cos_r64 - 6.2) < 1e-6);
       file->step();
       assert(sin_u8 == 5);
       assert(cos_u8 == 7);
@@ -102,6 +123,10 @@ int main(int argc, const char **argv)
       assert(cos_s32 == 129000);
       assert(sin_u32 == 1e7);
       assert(cos_u32 == 1e8);
+      assert(fabs(sin_r32 - 3.3) < 1e-6);
+      assert(fabs(cos_r32 - 4.4) < 1e-6);
+      assert(fabs(sin_r64 - 7.7) < 1e-6);
+      assert(fabs(cos_r64 - -8.8) < 1e-6);
       file->close();
   } catch (const char* msg) {
       std::cerr << msg << std::endl;
